864e6bfce06a70cb9790dcd64de6a9ef4a3b768e
Jan Scheers <jan@getmarple.io>
2022-12-03 00:43:33 +0100

    day 2

---
+++ av2022/src/Day2/Run.hs
@@ -0,0 +1,21 @@
+module Day2.Run (run, points) where
+import Lib (file, req)
+import Data.Char (ord)
+
+count :: ((Int, Int) -> Int) -> String -> Int
+count pts = sum.map (pts.parse).lines
+
+points :: (Int, Int) -> Int
+points (a, b) = b + 3 * ((b - a + 1) `mod` 3)
+
+points2 :: (Int, Int) -> Int
+points2 (a, b) = 3 * b - 2 + ((a + b) `mod` 3)
+
+parse :: String -> (Int, Int)
+parse s = ((ord $ s !! 0) - 64, (ord $ s !! 2) - 87)
+
+run :: IO ()
+run = do
+  r <- req 2
+  putStrLn.show.count points $ r
+  putStrLn.show.count points2 $ r

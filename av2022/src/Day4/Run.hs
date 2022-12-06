1380b04093b2fc31f743e34a06f15f046f829748
Jan Scheers <jan@getmarple.io>
2022-12-06 21:57:44 +0100

    day 5

---
+++ av2022/src/Day4/Run.hs
@@ -0,0 +1,26 @@
+module Day4.Run (run) where
+import Lib (req)
+import Data.List.Split (splitOn)
+
+parse :: String -> [Int]
+parse = (map read) . (concatMap $ splitOn "-").splitOn ","
+
+eval :: [Int] -> Int
+eval [a, b, c, d] = if a <= c && d <= b || c <= a && b <= d then 1 else 0
+eval _ = 0
+
+eval2 :: [Int] -> Int
+eval2 is@[a, b, c, d] = if eval is == 1 || a <= c && c <= b || a <= d && d <= b then 1 else 0
+eval2 _ = 0
+
+calc :: String -> Int
+calc = sum.map (eval.parse).lines
+
+calc2 :: String -> Int
+calc2 = sum.map (eval2.parse).lines
+
+run :: IO ()
+run = do
+    r <- req 4
+    putStrLn.show.calc $ r
+    putStrLn.show.calc2 $ r
---
+++ av2022/src/Day5/Run.hs
@@ -0,0 +1,42 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+module Day5.Run (run) where
+import Lib (req)
+
+parse :: String -> ([[Char]], [[Int]])
+parse s = let (stacks, is) = break (=="").lines $ s 
+    in (build stacks, map make $ tail is)
+
+build :: [[Char]] -> [[Char]]
+build sts = foldr 
+    (zipWith (\a b -> if a == ' ' then b else a:b)) 
+    (flip replicate [].length.head $ sts) 
+    (map (\lv -> map (lv !!) [1, 5 .. length lv]) $ init sts)
+
+
+make :: String -> [Int]
+make is = (\ws -> map (read.(ws !!)) [1, 3 .. length ws]) $ words is
+
+operate :: [[Char]] -> [Int] -> [[Char]]
+operate sts [n, one, two] = let (mv, from) = splitAt n $ sts !! (one - 1) 
+    in map (\(i, st) -> case () of 
+        _ | i + 1 == one -> from
+          | i + 1 == two -> reverse mv ++ st
+          | otherwise -> st) $ zip [0..] sts
+operate _ _ = []
+
+operate2 :: [[Char]] -> [Int] -> [[Char]]
+operate2 sts [n, one, two] = let (mv, from) = splitAt n $ sts !! (one - 1) 
+    in map (\(i, st) -> case () of 
+        _ | i + 1 == one -> from
+          | i + 1 == two -> mv ++ st
+          | otherwise -> st) $ zip [0..] sts
+operate2 _ _ = []
+
+run :: IO ()
+run = do
+    r <- req 5
+    let (sts, iss) = parse r
+    let (result) = foldl operate sts iss
+    putStrLn $ map head result
+    let (result2) = foldl operate2 sts iss
+    putStrLn $ map head result2

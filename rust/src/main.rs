684f54fdce48ea7c5bb63a36012403b81c2c7fdf
Jan Scheers <jan@getmarple.io>
2024-06-27 09:53:28 +0200

    day20

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    //let input = aoc::y22::day18::INPUT;
-    let input = &aoc::y22::get_day(18);
+    //let input = aoc::y22::day20::INPUT;
+    let input = &aoc::y22::get_day(20);
     //println!("{:?}", aoc::y22::day15::parse(input));
-    println!("{:?}", aoc::y22::day18::part_one(input));
-    println!("{:?}", aoc::y22::day18::part_two(input));
+    println!("{:?}", aoc::y22::day20::part_one(input));
+    println!("{:?}", aoc::y22::day20::part_two(input));
 }
--- rust/src/y22/day20.rs
+++ rust/src/y22/day20.rs
@@ -12,49 +12,73 @@ struct Item {
     shift: i64,
 }
 
-pub fn part_one() {
-    let mut items: Vec<Item> = vec![1, 2, -3, 3, -2, 0, 4]
-        .into_iter()
+type State = (Vec<Item>, Vec<usize>);
+
+fn parse(input: &str, key: i64) -> State {
+    let items: Vec<Item> = input
+        .lines()
+        .map(|s| s.parse::<i64>().unwrap())
         .enumerate()
         .map(|(index, shift)| Item {
-            index: index.try_into().unwrap(),
-            shift,
+            index: index as i64,
+            shift: shift * key,
         })
         .collect();
-    let mut table: Vec<usize> = (0..items.len()).collect();
-
-    println!(
-        "{:?}",
-        table.iter().map(|i| items[*i].shift).collect::<Vec<_>>()
-    );
-    for i in 0..7 {
-        shift(&mut items, &mut table, i);
-        println!(
-            "{:?}",
-            table.iter().map(|i| items[*i].shift).collect::<Vec<_>>()
-        );
-    }
+    let n = items.len();
+    (items, (0..n).collect())
 }
 
-fn shift(items: &mut Vec<Item>, table: &mut Vec<usize>, i: usize) {
-    let item = items[i].clone();
+pub fn part_one(input: &str) -> i64 {
+    let state = parse(input, 1);
+    let (items, table) = mix(state, 1);
+    let numbers: Vec<_> = table.into_iter().map(|i| items[i].shift).collect();
+    let index0 = numbers.iter().position(|&n| n == 0).unwrap();
+    vec![1000, 2000, 3000]
+        .into_iter()
+        .map(|i| numbers[(index0 + i) % numbers.len()])
+        .sum()
+}
+
+pub fn part_two(input: &str) -> i64 {
+    let state = parse(input, 811589153);
+    let (items, table) = mix(state, 10);
+    let numbers: Vec<_> = table.into_iter().map(|i| items[i].shift).collect();
+    let index0 = numbers.iter().position(|&n| n == 0).unwrap();
+    vec![1000, 2000, 3000]
+        .into_iter()
+        .map(|i| numbers[(index0 + i) % numbers.len()])
+        .sum()
+}
+
+fn mix(state: State, iter: usize) -> State {
+    (0..state.0.len() * iter).fold(state, shift)
+}
+
+fn shift((mut items, mut table): State, i: usize) -> State {
+    let i = i % items.len();
+    let item = &items[i];
     let len = items.len() as i64;
     let mut dest = (item.index + item.shift).rem_euclid(len - 1);
     if dest == 0 && item.shift != 0 {
         dest = len - 1;
     }
 
-    let mut ix = [item.index as usize, dest as usize];
-    ix.sort();
-    (ix[0]..ix[1]).for_each(|i| {
-        let (a, b) = if item.index < dest {
-            (i + 1, i)
-        } else {
-            (i, i + 1)
-        };
-        table[a] = table[b];
-        items[table[a]].index = a as i64;
-    });
+    if item.index < dest {
+        (item.index..dest).for_each(|i| {
+            let (curr, next) = (i as usize + 1, i as usize);
+            let item = table[curr];
+            table[next] = item;
+            items[item].index = next as i64;
+        })
+    } else {
+        (dest..item.index).rev().for_each(|i| {
+            let (curr, next) = (i as usize, i as usize + 1);
+            let item = table[curr];
+            table[next] = item;
+            items[item].index = next as i64;
+        })
+    }
     items[i].index = dest;
-    table[ix[1]] = i;
+    table[dest as usize] = i;
+    (items, table)
 }

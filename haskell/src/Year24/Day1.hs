3d5b80447ebad06b30458bf9f1dcdb3808d357e0
Jan Scheers <jan@getmarple.io>
2024-12-04 00:33:48 +0100

    day1

--- haskell/src/Year24/Day1.hs
+++ haskell/src/Year24/Day1.hs
@@ -9,9 +9,10 @@ import Data.Text qualified as T
 import Lib (requestDay)
 
 parse :: T.Text -> ([Int], [Int])
-parse = pairsToLists . map toTuple . T.lines
+parse = pairsToLists . map toIntPairs . T.lines
   where
-    toTuple x = let ws = T.words x in (read . T.unpack $ head ws, read . T.unpack $ (!! 1) ws)
+    toIntPairs = (\pair -> (toInt $ head pair, toInt $ last pair)) . T.words
+    toInt = read . T.unpack
     pairsToLists = foldr f ([], [])
     f (a, b) (as, bs) = (a : as, b : bs)
 
@@ -19,8 +20,9 @@ partOne :: ([Int], [Int]) -> Int
 partOne (as, bs) = sum $ zipWith (\a b -> abs (a - b)) (sort as) (sort bs)
 
 partTwo :: ([Int], [Int]) -> Int
-partTwo (as, bs) = sum $ map (\a -> a * fromMaybe 0 (count bs M.!? a)) as
+partTwo (as, bs) = sum $ map similarity as
   where
+    similarity a = a * fromMaybe 0 (count bs M.!? a)
     count = foldr f M.empty
     f x = M.insertWith (+) x 1
 

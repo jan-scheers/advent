a6581fe97d3f3a127057027c05db816e447a4855
Jan Scheers <jan@getmarple.io>
2025-12-06 22:32:41 +0100

    day 4

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let input = aoc::y25::get_day(4);
-    println!("{}", aoc::y25::day4::part_one(&input));
-    println!("{}", aoc::y25::day4::part_two(&input));
+    let input = aoc::y25::day5::INPUT;
+    println!("{}", aoc::y25::day5::part_one(&input));
+    //println!("{}", aoc::y25::day4::part_two(&input));
     //println!("{}", aoc::y25::day3::part_two(&input));
     //     "{}",
     //     Solution::smallest_equivalent_string(
--- rust/src/y25/mod.rs
+++ rust/src/y25/mod.rs
@@ -2,6 +2,7 @@ pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
+pub mod day5;
 
 const SESSION: &str = "_ga=GA1.2.849613819.1765045556; _gid=GA1.2.85157165.1765045556; _ga_MHSNPJKWC7=GS2.2.s1765045556$o1$g1$t1765045954$j60$l0$h0; session=53616c7465645f5f27a4a6ea630cef70bb822da39dbd9057bdcc38e89f16ffc15d52f50a1291dbd6685f6bf7f52df198df2f0ee474595ec2c57e9f06501427f8";
 
